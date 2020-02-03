0dc07f21d1b13174276f4b1a0973b8340b4a2eb6
Suffyan Tariq <suffyan.tariq@gmail.com>
2020-02-03 13:46:42 -0800

    added more file structure

---
+++ src/components/CustomerHome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import StylistList from './StylistList'
+
+function CustomerHome() {
+  return (
+    <StylistList/>
+  )   
+}
+
+export default CustomerHome;
---
+++ src/components/StylistHome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import StylistList from './StylistList'
+
+function StylistHome() {
+  return (
+    <StylistList/>
+  )   
+}
+
+export default StylistHome;
